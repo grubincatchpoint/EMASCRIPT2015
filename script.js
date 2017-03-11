2ab3287d3e2df206cd5f9d1c73b77700819d617b
grubincatchpoint <grubin@catchpoint.com>
2017-03-10 21:56:40 -0500

    Update script.js

--- script.js
+++ script.js
@@ -145,9 +145,9 @@ const box66 = {
 box66.clickMe();
 */
 
-function Person(name) {
-  this.name = name;
-}
+//function Person(name) {
+//  this.name = name;
+//}
 
 //ES5
 /*
@@ -338,3 +338,70 @@ function SmithPerson(firstName, yearOfBirth, lastName = 'Smith', nationality = '
 var john = new SmithPerson('John', 1990);
 var emily = new SmithPerson('Emily', 1983, 'Diaz', 'Spanish');
 */
+/////////////////////////////////////////////////////////////////////////////////
+//Maps for hashmaps
+/*
+const question = new Map();
+question.set('question', 'What is the official name of the latest major JavaScript version?');
+question.set(1, 'ES5');
+question.set(2, 'ES6');
+question.set(3, 'ES2015');
+question.set(4, 'ES7');
+question.set('correct', 3);
+question.set(true, 'Correct answer!');
+question.set(false, 'Wrong! LEARN JAVASCRIPT BRO!');
+console.log(question.get('question'));
+//console.log(question.size);
+//if (question.has(4)) {
+//  console.log('Question 4 exists');
+//}
+
+//question.clear();
+
+//question.forEach((value, key) => console.log(`This is ${key}, and it's set to ${value}`));
+
+for( let [key, value] of question.entries()) {
+  if (typeof(key) === 'number') {
+    console.log(`Answer ${key}: ${value}`);
+  }
+}
+const ans = parseInt(prompt('Write the correct answer'));
+console.log(question.get(ans === question.get('correct')));
+*/
+///////////////////////////////////////////////////////////////////////////////
+//Classes
+/*
+//ES5
+var Person5 = function(name, yearOfBirth, job) {
+  this.name = name;
+  this.yearOfBirth = yearOfBirth;
+  this.job = job;
+};
+
+Person5.prototype.calculateAge = function() {
+  var age = new Date().getUTCFullYear() - this.yearOfBirth;
+  console.log(age);
+};
+
+var john5 = new Person5('John', 1990, 'teacher');
+
+//ES6
+class Person6 {
+  constructor (name, yearOfBirth, job) {
+    this.name = name;
+    this.yearOfBirth = yearOfBirth;
+    this.job = job;
+  }
+  calculateAge() {
+    var age = new Date().getFullYear() - this.yearOfBirth;
+    console.log(age);
+  }
+  static greeting() {
+    console.log('Hey there!');
+  }
+}
+const john6 = new Person6('John', 1991, 'teacher');
+Person6.greeting();
+*/
+////////////////////////////////////////////////////////////////////////////////
+//Classes with subclasses
